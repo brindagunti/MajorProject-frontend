129d28c1b6be5f1438f55acfd3f0d16ea88a0bd1
Brinda <138833285+brindagunti@users.noreply.github.com>
2025-08-10 18:17:25 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter as Router } from 'react-router-dom';
+import AppRoutes from './routes/AppRoutes';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <AppRoutes />
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
